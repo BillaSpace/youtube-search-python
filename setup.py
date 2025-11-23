e872b98f5142ab4425117f06575354ada51b2a84
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-23 10:40:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="youtube-search-python",
-    version="1.6.6.post3",  # updated version to reflect httpx 0.28+ fix
-    author="CertifiedCoders (forked from Hitesh Kumar Saini)",
+    version="1.6.6.Master",
+    author="Prakhar Shukla (forked from Hitesh Kumar Saini)",
     license='MIT',
     author_email="rajnishmishraaa1@gmail.com",
     description="Search for YouTube content without YouTube Data API v3. Patched for httpx 0.28+.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CertifiedCoders/youtube-search-python",  # updated repo URL
+    url="https://github.com/BillaSpace/youtube-search-python",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'httpx>=0.28.1'  # fixed compatibility issue
+        'httpx>=0.28.1'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
