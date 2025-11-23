00e73f33e62de71d448e70784a13e69b40c27ffe
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-23 10:03:25 +0530

    Update channelsearch.py

--- youtubesearchpython/core/channelsearch.py
+++ youtubesearchpython/core/channelsearch.py
@@ -36,62 +36,58 @@ class ChannelSearchCore(RequestCore, ComponentHandler):
 
     def _parseChannelSearchSource(self) -> None:
         try:
-            last_tab = self.response["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][-1]
+            tabs = self.response.get("contents", {}).get("twoColumnBrowseResultsRenderer", {}).get("tabs", []) or []
+            last_tab = tabs[-1] if tabs else {}
             if 'expandableTabRenderer' in last_tab:
-                self.response = last_tab["expandableTabRenderer"]["content"]["sectionListRenderer"]["contents"]
+                self.response = last_tab["expandableTabRenderer"].get("content", {}).get("sectionListRenderer", {}).get("contents", [])
             else:
-                tab_renderer = last_tab["tabRenderer"]
-                if 'content' in tab_renderer:
-                    self.response = tab_renderer["content"]["sectionListRenderer"]["contents"]
+                tab_renderer = last_tab.get("tabRenderer", {})
+                content = tab_renderer.get("content")
+                if content and "sectionListRenderer" in content:
+                    self.response = content["sectionListRenderer"].get("contents", [])
                 else:
                     self.response = []
-        except:
+        except Exception:
             raise Exception('ERROR: Could not parse YouTube response.')
 
     def _getRequestBody(self):
-        ''' Fixes #47 '''
         requestBody = copy.deepcopy(requestPayload)
-        requestBody['query'] = self.query
-        requestBody['client'] = {
-            'hl': self.language,
-            'gl': self.region,
-        }
-        requestBody['params'] = self.searchPreferences
-        requestBody['browseId'] = self.browseId
-        self.url = 'https://www.youtube.com/youtubei/v1/browse' + '?' + urlencode({
-            'key': searchKey,
+        requestBody['query'] = self.query or ''
+        context = requestBody.setdefault('context', {})
+        client = context.setdefault('client', {})
+        client.update({
+            'hl': self.language or client.get('hl'),
+            'gl': self.region or client.get('gl'),
         })
+        if self.searchPreferences:
+            requestBody['params'] = self.searchPreferences
+        if self.browseId:
+            requestBody['browseId'] = self.browseId
+        if self.continuationKey:
+            requestBody['continuation'] = self.continuationKey
+        if not searchKey:
+            raise Exception('INNERTUBE API key (searchKey) is not set.')
+        self.url = 'https://www.youtube.com/youtubei/v1/browse' + '?' + urlencode({'key': searchKey})
         self.data = requestBody
 
     def _syncRequest(self) -> None:
-        ''' Fixes #47 '''
         self._getRequestBody()
-
         request = self.syncPostRequest()
         try:
-            self.response = request.json()
-        except:
+            self.response = request.json() if hasattr(request, "json") else json.loads(request.text)
+        except Exception:
             raise Exception('ERROR: Could not make request.')
 
     async def _asyncRequest(self) -> None:
-        ''' Fixes #47 '''
         self._getRequestBody()
-
         request = await self.asyncPostRequest()
         try:
-            self.response = request.json()
-        except:
+            self.response = request.json() if hasattr(request, "json") else json.loads(request.text)
+        except Exception:
             raise Exception('ERROR: Could not make request.')
 
     def result(self, mode: int = ResultMode.dict) -> Union[str, dict]:
-        '''Returns the search result.
-        Args:
-            mode (int, optional): Sets the type of result. Defaults to ResultMode.dict.
-        Returns:
-            Union[str, dict]: Returns JSON or dictionary.
-        '''
         if mode == ResultMode.json:
             return json.dumps({'result': self.response}, indent=4)
         elif mode == ResultMode.dict:
             return {'result': self.response}
-
