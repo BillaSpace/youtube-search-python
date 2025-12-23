732f2ecb68a8ddee9378ce99bd01a3d1a31c11e6
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 04:57:22 +0530

    Update extras.py

--- tests/async/extras.py
+++ tests/async/extras.py
@@ -1,60 +1,60 @@
 import asyncio
-from youtubesearchpython.__future__ import *
-
+from youtubesearchpython.__future__ import Video, StreamURLFetcher, Suggestions
+from youtubesearchpython import Hashtag, Comments, Transcript, Channel
 
 async def main():
     video = await Video.get('https://www.youtube.com/watch?v=z0GKGpObgPY', get_upload_date=True)
     print(video)
+    
     videoInfo = await Video.getInfo('https://youtu.be/z0GKGpObgPY')
     print(videoInfo)
+    
     videoFormats = await Video.getFormats('z0GKGpObgPY')
     print(videoFormats)
 
-
-    suggestions = await Suggestions.get('PyarHumara', language = 'en', region = 'US')
+    suggestions = await Suggestions.get('PyarHumara', language='en', region='US')
     print(suggestions)
 
-
-    hashtag = Hashtag('raw', limit = 1)
-    result = await hashtag.next()
+    hashtag = Hashtag('raw', limit=1)
+    result = await asyncio.to_thread(hashtag.result)
     print(result)
 
-
     fetcher = StreamURLFetcher()
-    await fetcher.getJavaScript()
+    
     videoA = await Video.get("https://www.youtube.com/watch?v=aqz-KE-bpKQ")
     videoB = await Video.get("https://www.youtube.com/watch?v=ZwNxYJfW-eU")
+    
+    await fetcher.getJavaScript()
+    
     singleUrlA = await fetcher.get(videoA, 22)
     allUrlsB = await fetcher.getAll(videoB)
     print(singleUrlA)
     print(allUrlsB)
 
-
     comments = Comments("_ZdsmLgCVdU")
-    await comments.getNextComments()
-    while len(comments.comments["result"]) < 100:
+    await asyncio.to_thread(comments.getNextComments)
+    while len(comments.comments["result"]) < 100 and comments.hasMoreComments:
         print(len(comments.comments["result"]))
-        await comments.getNextComments()
+        await asyncio.to_thread(comments.getNextComments)
     print("Found all comments")
 
+    transcript_en = await asyncio.to_thread(Transcript.get, "https://youtube.com?v=Um2PmRde4OQ")
+    print(transcript_en)
     
-    print(await Transcript.get("https://youtube.com?v=Um2PmRde4OQ"))
-
-
     url = "https://www.youtube.com/watch?v=-1xu0IP35FI"
-    transcript_en = await Transcript.get(url)
-    transcript_2 = await Transcript.get(url, transcript_en["languages"][-1]["params"])
-    print(transcript_2)
+    transcript_en = await asyncio.to_thread(Transcript.get, url)
+    if transcript_en.get("languages"):
+        transcript_2 = await asyncio.to_thread(Transcript.get, url, transcript_en["languages"][-1]["params"])
+        print(transcript_2)
 
-    print(await Channel.get("UC_aEa8K-EOJ3D6gOs7HcyNg"))
+    channel_info = await asyncio.to_thread(Channel.get, "UC_aEa8K-EOJ3D6gOs7HcyNg")
+    print(channel_info)
 
     channel = Channel("UC_aEa8K-EOJ3D6gOs7HcyNg")
-    await channel.init()
+    await asyncio.to_thread(channel.init)
     print(len(channel.result["playlists"]))
     while channel.has_more_playlists():
-        await channel.next()
+        await asyncio.to_thread(channel.next)
         print(len(channel.result["playlists"]))
 
-
 asyncio.run(main())
-
