65288086b8f0fbf0d73337ea8169dc297f717cfe
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 05:04:50 +0530

    Update extras.py

--- tests/async/extras.py
+++ tests/async/extras.py
@@ -6,7 +6,7 @@ async def main():
     video = await Video.get('https://www.youtube.com/watch?v=z0GKGpObgPY', get_upload_date=True)
     print(video)
     
-    videoInfo = await Video.getInfo('https://youtu.be/z0GKGpObgPY')
+    videoInfo = await Video.getInfo('https://youtu.be/GeWnEyQRCUs?si=8pUvXErVeZFXpILf')
     print(videoInfo)
     
     videoFormats = await Video.getFormats('z0GKGpObgPY')
@@ -15,7 +15,7 @@ async def main():
     suggestions = await Suggestions.get('PyarHumara', language='en', region='US')
     print(suggestions)
 
-    hashtag = Hashtag('raw', limit=1)
+    hashtag = Hashtag('IndianArmy', limit=1)
     result = await asyncio.to_thread(hashtag.result)
     print(result)
 
@@ -24,30 +24,28 @@ async def main():
     videoA = await Video.get("https://www.youtube.com/watch?v=aqz-KE-bpKQ")
     videoB = await Video.get("https://www.youtube.com/watch?v=ZwNxYJfW-eU")
     
-    await fetcher.getJavaScript()
-    
-    singleUrlA = await fetcher.get(videoA, 22)
+    singleUrlA = await fetcher.get(videoA, 137)
     allUrlsB = await fetcher.getAll(videoB)
     print(singleUrlA)
     print(allUrlsB)
 
     comments = Comments("_ZdsmLgCVdU")
     await asyncio.to_thread(comments.getNextComments)
-    while len(comments.comments["result"]) < 100 and comments.hasMoreComments:
+    while len(comments.comments["result"]) < 100 and getattr(comments, "hasMoreComments", False):
         print(len(comments.comments["result"]))
         await asyncio.to_thread(comments.getNextComments)
     print("Found all comments")
 
-    transcript_en = await asyncio.to_thread(Transcript.get, "https://youtube.com?v=Um2PmRde4OQ")
+    transcript_en = await asyncio.to_thread(Transcript.get, "https://youtu.be/89d02K5pIU8?si=z0NtBv6iV1Rc37Mq")
     print(transcript_en)
     
-    url = "https://www.youtube.com/watch?v=-1xu0IP35FI"
+    url = "https://youtu.be/89d02K5pIU8?si=z0NtBv6iV1Rc37Mq"
     transcript_en = await asyncio.to_thread(Transcript.get, url)
     if transcript_en.get("languages"):
         transcript_2 = await asyncio.to_thread(Transcript.get, url, transcript_en["languages"][-1]["params"])
         print(transcript_2)
 
-    channel_info = await asyncio.to_thread(Channel.get, "UC_aEa8K-EOJ3D6gOs7HcyNg")
+    channel_info = await asyncio.to_thread(Channel.get, "UC_aEa8K-EOJ3d6gOs7HcyNg")
     print(channel_info)
 
     channel = Channel("UC_aEa8K-EOJ3D6gOs7HcyNg")
