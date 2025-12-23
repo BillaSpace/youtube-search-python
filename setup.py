83fb610bd3edfef02495c8586cf26e858ea47cea
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 01:07:36 +0530

    upgrades

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="youtube-search-python",
-    version="1.6.6+master",
-    author="Prakhar Shukla (forked from CertifiedCoder < Hitesh Kumar Saini)",
+    version="1.6.9+master",
+    author="Prakhar Shukla",
     license='MIT',
-    author_email="rajnishmishraaa1@gmail.com",
-    description="Search for YouTube content without YouTube Data API v3. Patched for httpx 0.28+.",
-    long_description="Search for YouTube content without YouTube Data API v3. Patched for httpx 0.28+.",
+    author_email="srvopus@gmail.com",
+    description="Modern Search for YouTube content without need of YouTube Data API v3. Maintained fork with modern Python support.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BillaSpace/youtube-search-python",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'httpx>=0.28.1'
+        'httpx>=0.28.1'  # compatible with httpx 0.28+
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',  # Python 3.6 is EOL, upgraded to 3.7+
 )
