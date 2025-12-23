20ad957f8452f36f9fe428031efc50eb79f429b2
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 02:56:39 +0530

    Update search.py

--- tests/async/search.py
+++ tests/async/search.py
@@ -1,31 +1,27 @@
-from youtubesearchpython.__future__ import *
 import asyncio
+from youtubesearchpython.aio import *
+
 
 async def main():
-    search = Search('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
+    search = Search('TeraZikr', limit=19, language='en', region='US')
     result = await search.next()
     print(result)
 
-
-    videosSearch = VideosSearch('NoCopyrightSounds', limit = 10, language = 'en', region = 'US')
+    videosSearch = VideosSearch('HumnavaMere', limit=10, language='en', region='US')
     videosResult = await videosSearch.next()
     print(videosResult)
 
-
-    channelsSearch = ChannelsSearch('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
+    channelsSearch = ChannelsSearch('T-Series', limit=5, language='en', region='US')
     channelsResult = await channelsSearch.next()
     print(channelsResult)
 
-
-    playlistsSearch = PlaylistsSearch('NoCopyrightSounds', limit = 1, language = 'en', region = 'US')
+    playlistsSearch = PlaylistsSearch('BollywoodHipHop/TrapRemixes', limit=1, language='en', region='US')
     playlistsResult = await playlistsSearch.next()
     print(playlistsResult)
 
-
-    customSearch = CustomSearch('NoCopyrightSounds', VideoSortOrder.uploadDate, language = 'en', region = 'US')
+    customSearch = CustomSearch('NoCopyrightSounds', VideoSortOrder.uploadDate, language='en', region='US')
     customResult = await customSearch.next()
     print(customResult)
-
 
     search = ChannelSearch('Watermelon Sugar', "UCZFWPqqPkFlNwIxcpsLOwew")
     result = await search.next()
@@ -35,18 +31,7 @@ async def main():
     result = await channel.next()
     print(result)
 
-    """
-    channel = ChannelPlaylistSearch('PewDiePie', 'UC-lHJZR3Gqxm24_Vd_AJ5Yw')
-    result = await channel.next()
-    print(result)
-
-    channel = ChannelPlaylistSearch('The Beatles - Topic', 'UC2XdaAVUannpujzv32jcouQ')
-    result = await channel.next()
-    print(result)
-    """
-
-
-    search = VideosSearch('NoCopyrightSounds')
+    search = VideosSearch('PalPal')
     index = 0
     result = await search.next()
     for video in result['result']:
@@ -60,6 +45,5 @@ async def main():
     for video in result['result']:
         index += 1
         print(f'{index} - {video["title"]}')
-
 
 asyncio.run(main())
