e4fce572b371423307beec35b4ae93f2e2703276
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 12:42:56 +0530

    Update utils.py

--- youtubesearchpython/core/utils.py
+++ youtubesearchpython/core/utils.py
@@ -7,6 +7,5 @@ def playlist_from_channel_id(channel_id: str) -> str:
     elif cid.startswith("U"):
         list_id = "UU" + cid[1:]
     else:
-        # assume raw id given without prefix
         list_id = "UU" + cid
     return f"https://www.youtube.com/playlist?list={list_id}"
