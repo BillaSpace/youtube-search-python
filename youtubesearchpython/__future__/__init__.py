6aa5e560fa248298bf834031308153634931a14b
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 15:21:22 +0530

    Update __init__.py

--- youtubesearchpython/__future__/__init__.py
+++ youtubesearchpython/__future__/__init__.py
@@ -1,11 +1,11 @@
-from youtubesearchpython.__future__.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
-from youtubesearchpython.__future__.extras import Video, Playlist, Suggestions, Hashtag, Comments, Transcript, Channel
-from youtubesearchpython.__future__.streamurlfetcher import StreamURLFetcher
+from youtubesearchpython.aio.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
+from youtubesearchpython.aio.extras import Video, Playlist, Suggestions, Hashtag, Comments, Transcript, Channel
+from youtubesearchpython.aio.streamurlfetcher import StreamURLFetcher
 from youtubesearchpython.core.utils import *
 from youtubesearchpython.core.constants import *
 
 
 __title__        = 'youtube-search-python'
-__version__      = '2.0.0+master'
+__version__      = '2.0.0+master'    
 __author__       = 'Prakhar'
 __license__      = 'MIT'
