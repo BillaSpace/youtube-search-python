e34f8cfdce0ab96591aff36f787c7fb0db236176
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-24 13:18:59 +0530

    bumped latest web clients >
    switched to latest web version for less elements recalls , optimisations

--- youtubesearchpython/core/video.py
+++ youtubesearchpython/core/video.py
@@ -20,7 +20,7 @@ CLIENTS = {
         'context': {
             'client': {
                 'clientName': 'WEB',
-                'clientVersion': '2.20210224.06.00',
+                'clientVersion': '2.20241210.01.00',
                 'newVisitorCookie': True
             },
             'user': {
