8f366a24bc0cf440a6393f04c0533f10b743fd42
Prakhar Shukla <SmokenX1@gmail.com>
2025-12-28 03:42:17 +0530

    Update requesthandler.py

--- youtubesearchpython/handlers/requesthandler.py
+++ youtubesearchpython/handlers/requesthandler.py
@@ -1,70 +1,63 @@
 from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 from urllib.parse import urlencode
 import json
 import copy
-from youtubesearchpython.handlers.componenthandler import ComponentHandler
+from youtubesearchpython.core.componenthandler import ComponentHandler
 from youtubesearchpython.core.constants import *
+from youtubesearchpython.core.exceptions import YouTubeRequestError, YouTubeParseError
 
 
 class RequestHandler(ComponentHandler):
     def _makeRequest(self) -> None:
+        ''' Fixes #47 '''
         requestBody = copy.deepcopy(requestPayload)
-        requestBody['query'] = self.query or ''
-        context = requestBody.setdefault('context', {})
-        client = context.setdefault('client', {})
-        client.update({
-            'hl': self.language or client.get('hl'),
-            'gl': self.region or client.get('gl'),
-        })
+        requestBody['query'] = self.query
+        requestBody['client'] = {
+            'hl': self.language,
+            'gl': self.region,
+        }
         if self.searchPreferences:
             requestBody['params'] = self.searchPreferences
         if self.continuationKey:
             requestBody['continuation'] = self.continuationKey
-
-        if not searchKey:
-            raise Exception('INNERTUBE API key (searchKey) is not set.')
-
-        requestBodyBytes = json.dumps(requestBody).encode('utf-8')
-        url = 'https://www.youtube.com/youtubei/v1/search' + '?' + urlencode({'key': searchKey})
+        requestBodyBytes = json.dumps(requestBody).encode('utf_8')
         request = Request(
-            url,
-            data=requestBodyBytes,
-            headers={
+            'https://www.youtube.com/youtubei/v1/search' + '?' + urlencode({
+                'key': searchKey,
+            }),
+            data = requestBodyBytes,
+            headers = {
                 'Content-Type': 'application/json; charset=utf-8',
+                'Content-Length': len(requestBodyBytes),
                 'User-Agent': userAgent,
             }
         )
         try:
-            self.response = urlopen(request, timeout=self.timeout).read().decode('utf-8')
+            self.response = urlopen(request, timeout=self.timeout).read().decode('utf_8')
+        except (URLError, HTTPError, TimeoutError) as e:
+            raise YouTubeRequestError(f'Failed to make request: {str(e)}')
         except Exception as e:
-            raise Exception('ERROR: Could not make request.') from e
-
+            raise YouTubeRequestError(f'Unexpected error making request: {str(e)}')
+    
     def _parseSource(self) -> None:
         try:
-            data = json.loads(self.response)
             if not self.continuationKey:
-                responseContent = self._getValue(data, contentPath)
+                responseContent = self._getValue(json.loads(self.response), contentPath)
             else:
-                responseContent = self._getValue(data, continuationContentPath)
-
+                responseContent = self._getValue(json.loads(self.response), continuationContentPath)
             if responseContent:
                 for element in responseContent:
-                    if itemSectionKey in element:
+                    if itemSectionKey in element.keys():
                         self.responseSource = self._getValue(element, [itemSectionKey, 'contents'])
-                    if continuationItemKey in element:
+                    if continuationItemKey in element.keys():
                         self.continuationKey = self._getValue(element, continuationKeyPath)
             else:
-                fallback = self._getValue(data, fallbackContentPath)
-                if fallback:
-                    self.responseSource = fallback
-                    # attempt to set continuationKey from the last element if present there was url failing like for tracks like pal pal afusic
-                    try:
-                        last = self.responseSource[-1]
-                        self.continuationKey = self._getValue(last, continuationKeyPath)
-                    except Exception:
-                        self.continuationKey = None
-                else:
-                    self.responseSource = []
-                    self.continuationKey = None
+                self.responseSource = self._getValue(json.loads(self.response), fallbackContentPath)
+                self.continuationKey = self._getValue(self.responseSource[-1], continuationKeyPath)
+        except json.JSONDecodeError as e:
+            raise YouTubeParseError(f'Failed to parse JSON response: {str(e)}')
+        except KeyError as e:
+            raise YouTubeParseError(f'Missing expected key in response: {str(e)}')
         except Exception as e:
-            raise Exception('ERROR: Could not parse YouTube response.') from e
+            raise YouTubeParseError(f'Failed to parse YouTube response: {str(e)}')
