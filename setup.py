bb20dbee77bae4da22157ca41d9ba89e19e03708
Prakhar Shukla <SmokenX1@gmail.com>
2026-07-30 05:47:20 +0530

    ..:)
    Signed-off-by: Prakhar Shukla <SmokenX1@gmail.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+import setuptools
+from pathlib import Path
+
+long_description=Path("README.md").read_text(encoding="utf-8")
+
 setuptools.setup(
     name="yt-search-python",
     version="2.1.1",
@@ -15,20 +20,9 @@ setuptools.setup(
         "httpx>=0.28.1"
     ],
     extras_require={
-        "transcript": [
+        "transcript":[
             'yt-dlp; python_version >= "3.10"'
         ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
+    python_requires=">=3.7"
 )
